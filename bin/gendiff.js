a2b98034b6fd6e5b92161e87d7964b2de9e13158
Sergey Larionov <sergunya@outlook.com>
2022-02-22 10:56:59 +0100

    Add print diff for flat objects

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import { Command } from "commander";
+import { Command } from 'commander';
+import compareObjects from '../src/index.js';
 
 const program = new Command();
 
@@ -8,6 +9,9 @@ program
   .description('Compares two configuration files and shows a difference.')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    compareObjects(filepath1, filepath2);
+  });
 
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,60 @@
+import { readFileSync } from 'fs';
+import path from 'path';
+import _ from 'lodash';
+
+const printDiff = (obj1, obj2) => {
+  const mergedObject = { ...obj1, ...obj2 };
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  const allKeys = _.sortBy(Object.keys(mergedObject));
+  const result = ['{'];
+
+  allKeys.forEach((key) => {
+    if (keys1.includes(key) && keys2.includes(key)) {
+      if (obj1[key] === obj2[key]) {
+        result.push(`    ${key}: ${obj2[key]}`);
+      } else {
+        result.push(`  - ${key}: ${obj1[key]}`);
+        result.push(`  + ${key}: ${obj2[key]}`);
+      }
+    }
+
+    if (keys1.includes(key) && !keys2.includes(key)) {
+      result.push(`  - ${key}: ${obj1[key]}`);
+    }
+
+    if (!keys1.includes(key) && keys2.includes(key)) {
+      result.push(`  + ${key}: ${obj1[key]}`);
+    }
+  });
+
+  result.push('}');
+  console.log(result.join('\n'));
+};
+
+const getUnifiedFilePath = (filepath) => {
+  if (filepath.startsWith('.')) {
+    const procDir = process.cwd();
+
+    return path.resolve(procDir, filepath);
+  }
+
+  return path.resolve(filepath);
+};
+
+const getFileContent = (filepath) => {
+  const content = readFileSync(
+    getUnifiedFilePath(filepath),
+    { encoding: 'utf8', flag: 'r' },
+  );
+
+  return JSON.parse(content);
+};
+
+const compareObjects = (src1, src2) => {
+  const content1 = getFileContent(src1);
+  const content2 = getFileContent(src2);
+  printDiff(content1, content2);
+};
+
+export default compareObjects;
