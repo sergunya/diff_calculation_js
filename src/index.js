64b2dad2ef50ddbfad3979690842e186882f4895
Sergey Larionov <sergunya@outlook.com>
2022-02-24 10:21:41 +0100

    Start to implement comparisson not flat objects

--- src/index.js
+++ src/index.js
@@ -2,6 +2,51 @@ import _ from 'lodash';
 import proccedFile from './file.js';
 import parseFile from './parsers.js';
 
+const isObject = (obj) => {
+  if (typeof obj === 'object' && !Array.isArray(obj) && obj !== null) {
+    return true;
+  }
+
+  return false;
+};
+
+const stateOfValue = (key, obj1, obj2) => {
+  const existsInObj1 = Object.hasOwn(obj1, key);
+  const existsInObj2 = Object.hasOwn(obj2, key);
+  let state = 'remained';
+
+  if (existsInObj1 && existsInObj2 && obj1[key] !== obj2[key]) {
+    state = 'updated';
+  }
+
+  if (existsInObj1 && !existsInObj2) {
+    state = 'deleted';
+  }
+
+  if (!existsInObj1 && existsInObj2) {
+    state = 'added';
+  }
+
+  return state;
+};
+
+const getDiff2 = (obj1, obj2) => {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  const allKeys = [...new Set([...keys1, ...keys2])];
+  const diff = {};
+
+  allKeys.forEach((key) => {
+    if (isObject(obj2[key]) && isObject(obj1[key])) {
+      diff[key] = getDiff2(obj1[key], obj2[key]);
+    } else {
+      diff[key] = stateOfValue(key, obj1, obj2);
+    }
+  });
+
+  return diff;
+};
+
 const getDiff = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
@@ -37,6 +82,7 @@ const compareFiles = (src1, src2) => {
   const parsedFile1 = parseFile(file1.data, file1.extenstion);
   const parsedFile2 = parseFile(file2.data, file2.extenstion);
 
+  console.log(JSON.stringify(getDiff2(parsedFile1, parsedFile2)));
   return getDiff(parsedFile1, parsedFile2);
 };
 
