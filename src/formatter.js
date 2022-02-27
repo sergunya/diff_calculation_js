63a220fbbc0d9e278a4d33c94be84cffffc7f9f9
Sergey Larionov <sergunya@outlook.com>
2022-02-27 14:49:58 +0100

    Fix indent in reformatted strings

--- src/formatter.js
+++ src/formatter.js
@@ -16,14 +16,21 @@ const isDiffNode = (node) => {
   return false;
 };
 
+const getIndent = (level, state = ' ') => {
+  const baseIndent = '    ';
+  const indentLength = (baseIndent.length * level) - state.length;
+
+  return ' '.repeat(indentLength);
+};
+
 const stylish = (diff) => {
   const styleNode = (node, level) => {
     const keys = _.sortBy(Object.keys(node));
-    const indent = '  '.repeat(level);
+    let indent = getIndent(level);
 
     const result = keys.map((key) => {
       if (!isObject(node[key])) {
-        return `${indent}${key}: ${node[key]}`;
+        return `${indent} ${key}: ${node[key]}`;
       }
 
       if (isDiffNode(node[key])) {
@@ -33,30 +40,32 @@ const stylish = (diff) => {
         if (state === '±') {
           const { oldValue } = node[key];
           const formatNode = [];
+          indent = getIndent(level, STATES.deleted);
 
           if (isObject(oldValue)) {
-            formatNode.push([`${indent}${STATES.deleted} ${key}: {`, ...styleNode(oldValue, level + 1), `${indent}}`]);
+            formatNode.push([`${indent}${STATES.deleted} ${key}: {`, ...styleNode(oldValue, level + 1), `  ${indent}}`]);
           } else {
             formatNode.push(`${indent}${STATES.deleted} ${key}: ${oldValue}`);
           }
 
           if (isObject(value)) {
-            formatNode.push([`${indent}${state} ${key}: {`, ...styleNode(value, level + 1), `${indent}}`]);
+            formatNode.push([`${indent}${state.created} ${key}: {`, ...styleNode(value, level + 1), `  ${indent}}`]);
           } else {
             formatNode.push(`${indent}${STATES.created} ${key}: ${value}`);
           }
 
           return formatNode.flat();
         }
-
-        if (isObject(value)) {
-          return [`${indent}${state} ${key}: {`, ...styleNode(value, level + 1), `${indent}}`];
+        
+        if (isObject(value)) {          
+          indent = getIndent(level, state);
+          return [`${indent}${state} ${key}: {`, ...styleNode(value, level + 1), `  ${indent}}`];
         }
-
+        
         return `${indent}${state} ${key}: ${node[key].value}`;
       }
-
-      return [`${indent}${key}: {`, ...styleNode(node[key], level + 1), `${indent}}`];
+      
+      return [`${indent}${STATES.remained} ${key}: {`, ...styleNode(node[key], level + 1), `  ${indent}}`];
     });
 
     return result.flat();
