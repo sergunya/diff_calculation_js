64e2c90a80957a0b8f0ee47854885f74666097e2
Sergey Larionov <sergunya@outlook.com>
2022-03-01 11:18:56 +0100

    Fix code style issue

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,8 +1,6 @@
-import isObject from "../is_object.js";
 import _ from 'lodash';
+import isObject from '../is_object.js';
 
-const formatToJSON = (diff) => {
-    return JSON.stringify(diff);
-};
+const formatToJSON = (diff) => JSON.stringify(diff);
 
 export default formatToJSON;
