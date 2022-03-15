f37fa2307761edbf0ae260c26f0f98130805a969
Sergey Larionov <sergunya@outlook.com>
2022-03-15 18:05:49 +0100

    Change structure for difference between two obj-s

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,7 +2,6 @@ import parseFile from './parsers.js';
 import getFormatter from './formatters/index.js';
 import makeDiff from './diff.js';
 
-
 const genDiff = (src1, src2, formatName) => {
   const parsedFile1 = parseFile(src1);
   const parsedFile2 = parseFile(src2);
