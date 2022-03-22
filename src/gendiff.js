2dc6691612776a76821b646e0a53b97e362b3c0e
Sergey Larionov <sergunya@outlook.com>
2022-03-22 09:19:51 +0100

    Extract related to file functions to index.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,10 +1,17 @@
 import parseFile from './parsers.js';
 import getFormatter from './formatters/index.js';
 import makeDiff from './diff.js';
+import { getAbsoluteFilePath, getExtension, readFile } from '../index.js';
 
 const genDiff = (src1, src2, formatName) => {
-  const parsedFile1 = parseFile(src1);
-  const parsedFile2 = parseFile(src2);
+  const path1 = getAbsoluteFilePath(src1);
+  const path2 = getAbsoluteFilePath(src2);
+  const ext1 = getExtension(path1);
+  const ext2 = getExtension(path2);
+  const data1 = readFile(path1);
+  const data2 = readFile(path2);
+  const parsedFile1 = parseFile(data1, ext1);
+  const parsedFile2 = parseFile(data2, ext2);
 
   const diff = makeDiff(parsedFile1, parsedFile2);
   const formatter = getFormatter(formatName);
--- src/parsers.js
+++ src/parsers.js
@@ -1,37 +1,22 @@
 import yaml from 'js-yaml';
-import path from 'path';
-import { readFileSync } from 'fs';
-
-const getAbsoluteFilePath = (filepath) => {
-  if (filepath.startsWith('.')) {
-    const procDir = process.cwd();
-
-    return path.resolve(procDir, filepath);
-  }
-
-  return path.resolve(filepath);
-};
 
 const choosParser = (extension) => {
   switch (extension) {
-    case '.yml':
+    case 'yml':
       return yaml.load;
-    case '.yaml':
+    case 'yaml':
       return yaml.load;
-    case '.json':
+    case 'json':
       return JSON.parse;
     default:
       throw new Error(`Parser for file extension ${extension} not found`);
   }
 };
 
-const parseFile = (filepath) => {
-  const absolutePath = getAbsoluteFilePath(filepath);
-  const content = readFileSync(absolutePath, { encoding: 'utf8', flag: 'r' });
+const parseFile = (data, extension) => {
+  const parse = choosParser(extension);
 
-  const parse = choosParser(path.extname(absolutePath));
-
-  return parse(content);
+  return parse(data);
 };
 
 export default parseFile;
