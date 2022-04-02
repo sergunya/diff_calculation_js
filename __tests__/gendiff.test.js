260b8c0f1492e9fb4bc806523aaaa17d6881def5
Sergey Larionov <sergunya@outlook.com>
2022-04-02 22:58:17 +0200

    Reduce test code

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -14,38 +14,14 @@ const resultPlain = readFileSync(getPathToFile('resultPlain'), 'utf8');
 
 const extensions = ['json', 'yml'];
 
-describe('Test stylish formatter', () => {
-  test.each(extensions)('Check stylish formatter for %p extenstion', (extension) => {
+describe('Test formatters', () => {
+  test.each(extensions)('Check formatter for %p extenstion', (extension) => {
     const beforeFile = getPathToFile(`before.${extension}`);
     const afterFile = getPathToFile(`after.${extension}`);
 
     expect(gendiff(beforeFile, afterFile, 'stylish')).toEqual(resultStylish);
-  });
-});
-
-describe('Test JSON formatter', () => {
-  test.each(extensions)('Check JSON formatter for %p extenstion', (extension) => {
-    const beforeFile = getPathToFile(`before.${extension}`);
-    const afterFile = getPathToFile(`after.${extension}`);
-
     expect(gendiff(beforeFile, afterFile, 'json')).toEqual(resultJson);
-  });
-});
-
-describe('Test plain formatter', () => {
-  test.each(extensions)('Check plain formatter for %p extenstion', (extension) => {
-    const beforeFile = getPathToFile(`before.${extension}`);
-    const afterFile = getPathToFile(`after.${extension}`);
-
     expect(gendiff(beforeFile, afterFile, 'plain')).toEqual(resultPlain);
-  });
-});
-
-describe('Test default formatter', () => {
-  test.each(extensions)('Check default formatter for %p extenstion', (extension) => {
-    const beforeFile = getPathToFile(`before.${extension}`);
-    const afterFile = getPathToFile(`after.${extension}`);
-
     expect(gendiff(beforeFile, afterFile)).toEqual(resultStylish);
   });
 });
